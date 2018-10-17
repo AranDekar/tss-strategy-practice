b6e2f4175ca66973f2f51daf06b48c6320b5b997
Aran Dekar <aran.dekar@gmail.com>
2018-10-18 10:12:47 +1100

    feat(delete):added delete

--- src/services/StrategyService.js
+++ src/services/StrategyService.js
@@ -1,12 +1,12 @@
-import Strategy from '../schemas/Strategy';
-import StrategyReportSummary from '../schemas/StrategyReportSummary';
-import StrategyEvent from '../schemas/StrategyEvent';
-import StrategyReport from '../schemas/StrategyReport';
+import Strategy from "../schemas/Strategy";
+import StrategyReportSummary from "../schemas/StrategyReportSummary";
+import StrategyEvent from "../schemas/StrategyEvent";
+import StrategyReport from "../schemas/StrategyReport";
 
 class StrategyService {
   static async create(strategy) {
     if (!strategy.postedBy) {
-      throw new Error('❌ strategy is not properly sent');
+      throw new Error("❌ strategy is not properly sent");
     }
     const model = new Strategy(strategy);
     return model.save();
@@ -22,36 +22,43 @@ class StrategyService {
   }
 
   static async delete(strategyId) {
-    await StrategyEvent.deleteMany({ strategy: this.strategyId, instrument: instr }).exec();
-    await StrategyReport.deleteMany({ strategy: this.strategyId, instrument: instr }).exec();
-    await StrategyReportSummary.deleteMany({ strategy: this.strategyId, instrument: instr }).exec();
+    await StrategyEvent.deleteMany({ strategy: strategyId }).exec();
+    await StrategyReport.deleteMany({ strategy: strategyId }).exec();
+    await StrategyReportSummary.deleteMany({ strategy: strategyId }).exec();
     return Strategy.findByIdAndRemove(strategyId).exec();
   }
 
-
   /**
-  * Get yearly report of a backtest
-  */
+   * Get yearly report of a backtest
+   */
   static async getYearlyReport(strategy) {
     return StrategyReportSummary.where({ strategy })
-      .select('instrument year total maxProfit maxLoss').exec();
+      .select("instrument year total maxProfit maxLoss")
+      .exec();
   }
 
   /**
-  * Get yearly report of a backtest
-  */
+   * Get yearly report of a backtest
+   */
   static async getMonthlyReport(strategy, instrument) {
     const result = [];
     const data = await StrategyReportSummary.where({ strategy, instrument })
-      .select('instrument year monthly.month monthly.total monthly.maxProfit  monthly.maxLoss').exec();
+      .select(
+        "instrument year monthly.month monthly.total monthly.maxProfit  monthly.maxLoss"
+      )
+      .exec();
     for (const item of data) {
       const { year } = item;
       for (const monthly of item.monthly) {
-        const {
-          month, total, maxProfit, maxLoss,
-        } = monthly;
+        const { month, total, maxProfit, maxLoss } = monthly;
         result.push({
-          strategy, instrument, year, month, total, maxProfit, maxLoss,
+          strategy,
+          instrument,
+          year,
+          month,
+          total,
+          maxProfit,
+          maxLoss
         });
       }
     }
